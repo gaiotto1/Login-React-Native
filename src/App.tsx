395e1a8a31069c2c1eeaa9f8e8deb18910f17110
gaiotto1 <juliogaiotto@hotmail.com>
2021-03-26 15:06:17 -0300

    Configurando a StatusBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import 'react-native-gesture-handler';
 
 import React from 'react';
+import {StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 
 import Routes from './routes';
 
 const App: React.FC = () => (
-  <NavigationContainer>
-    <Routes />
-  </NavigationContainer>
+  <>
+    <StatusBar translucent backgroundColor="transparent" />
+    <NavigationContainer>
+      <Routes />
+    </NavigationContainer>
+  </>
 );
 
 export default App;
